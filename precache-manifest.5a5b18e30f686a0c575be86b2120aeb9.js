92d0a9e74e7802043e411d60f914b55e6267ce42
fekasgt <fekasgt@users.noreply.github.com>
2020-01-27 09:30:09 +0100

    Updates

---
+++ precache-manifest.5a5b18e30f686a0c575be86b2120aeb9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88cb264e67421b0803ca34eb72f1275b",
+    "url": "/Robofriends/index.html"
+  },
+  {
+    "revision": "7f82860ef3dce4d9acdc",
+    "url": "/Robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "061ef813a0c765d019bf",
+    "url": "/Robofriends/static/css/main.1ad68760.chunk.css"
+  },
+  {
+    "revision": "7f82860ef3dce4d9acdc",
+    "url": "/Robofriends/static/js/2.2a21f297.chunk.js"
+  },
+  {
+    "revision": "061ef813a0c765d019bf",
+    "url": "/Robofriends/static/js/main.b815bbba.chunk.js"
+  },
+  {
+    "revision": "78cab9b9619ddcd920c7",
+    "url": "/Robofriends/static/js/runtime-main.ff6ebadc.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
